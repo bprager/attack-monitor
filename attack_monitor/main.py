fe52e8aebec893ce9cc275f47e5fd6740ec19173
Bernd Prager <bernd@prager.ws>
2023-10-12 10:26:54 -0700

    completed version

--- attack_monitor/main.py
+++ attack_monitor/main.py
@@ -1,29 +1,94 @@
 #!/usr/bin/env python3
 import locale
-from datetime import datetime
+import logging
 import sqlite3
+import geoip2.database
+import os
+import subprocess
+import re
+from datetime import datetime
 
 from .mode import Mode
 
 DATABASE = "../attack.db"
+GEO_DB = "/usr/share/GeoIP/GeoLite2-City.mmdb"
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 locale.setlocale(locale.LC_ALL, "")
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(name)s - %(funcName)s:%(lineno)d: %(message)s",
+    handlers=[logging.FileHandler(f"{SCRIPT_DIR}/attack_monitor.log")],
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+def location(ip: str) -> str:
+    country = ""
+    city = ""
+    with geoip2.database.Reader(GEO_DB) as reader:
+        try:
+            response = reader.city(ip)
+            country = (
+                "Unknow" if response.country.name == None else response.country.name
+            )
+            city = "Unknown" if response.city.name == None else response.city.name
+        except geoip2.errors.AddressNotFoundError:
+            country = "Unknow"
+            city = "Unknown"
+
+    return f"{city} ({country})"
 
 
 class Record:
     ip: str
     location: str
-    blocked: bool
     last: datetime
+    blocked: bool
     avg: float
     num: int
+
+    def __init__(
+        self,
+        ip: str = "",
+        location: str = "",
+        last: datetime = datetime.now(),
+        blocked: bool = False,
+        avg: float = 0.0,
+        num: int = 0,
+    ):
+        self.ip = ip
+        self.location = location
+        self.last = last
+        self.blocked = blocked
+        self.avg = avg
+        self.num = num
+
+    def __repr__(self):
+        return f"ip:{self.ip} location:{self.location} blocked: {self.blocked} last: {self.last} avg: {self.avg} num: {self.num} "
 
 
 class Data:
     total: int
     since: datetime
     last: datetime
-    reords: list[Record]
+    records: list[Record]
+
+    def __init__(
+        self,
+        total: int = 0,
+        since: datetime = datetime.now(),
+        last: datetime = datetime.now(),
+        records: list[Record] = Record(),
+    ):
+        self.total = total
+        self.since = since
+        self.last = last
+        self.records = records
+
+    def __repr__(self) -> str:
+        return f"total: {self.total} since: {self.since} last: {self.last} records: {self.records}"
 
 
 class Source:
@@ -40,21 +105,49 @@ class Source:
         print(3)
         self.data = Data()
 
-    def get(self, mode: Mode) -> Data:
+    def get(self, mode: Mode, rows: int) -> Data:
         self.mode = mode
+        # Get blocked IP addresses
+        output = subprocess.getoutput("sudo ipset save -o plain")
+        blocked = re.findall(r"[\w:.]+", output)
+        if "Members:" in blocked:
+            start = blocked.index("Members:")
+        blocked = blocked[start + 1 :]
+        loggging.debug(f"blocked: {blocked}")
         with sqlite3.connect(database=self.db_string) as con:
             cur = con.cursor()
+            # stats
             cur.execute("SELECT sum(numbers), max(last), min(first) FROM attacks")
             self.data.total, tstamp_last, tstamp_first = cur.fetchone()
             self.data.last = datetime.fromtimestamp(tstamp_last)
             self.data.first = datetime.fromtimestamp(tstamp_first)
+            # records
+            self.data.records = []
+            sort_by = ["last", "avg", "numbers"][mode.value]
+            if sort_by == "avg":
+                cur.execute(
+                    f"SELECT ip, last, avg, numbers FROM attacks WHERE avg IS NOT NULL AND avg > 0 ORDER BY avg ASC NULLS LAST LIMIT {rows}"
+                )
+            else:
+                cur.execute(
+                    f"SELECT ip, last, avg, numbers FROM attacks ORDER BY {sort_by} DESC NULLS FIRST LIMIT {rows}"
+                )
+            for row in cur.fetchall():
+                record = Record()
+                record.ip, tstamp, record.avg, record.num = row
+                if record.avg == None:
+                    record.avg = 0.0
+                record.last = datetime.fromtimestamp(tstamp)
+                record.location = location(record.ip)
+                record.blocked = record.ip in blocked
+                self.data.records.append(record)
         return self.data
 
 
 def main():
     source = Source(DATABASE)
     mode = Mode.TIME
-    source.get(mode)
+    source.get(mode, 10)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -5,9 +5,18 @@ import logging
 import os
 import time
 from curses import wrapper
+from enum import Enum
+
+
+class Theme(Enum):
+    WHITE_ON_BLACK = 1
+    RED_ON_BLACK = 2
+    CYAN_ON_BLACK = 3
+    BLACK_ON_CYAN = 4
+
 
 DATABASE = "./attack.db"
-REFRESH = 2
+REFRESH = 5
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 logging.basicConfig(
@@ -18,33 +27,28 @@ logging.basicConfig(
 )
 
 
-def draw_screen(screen, data, rows, cols):
-    # colors
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_CYAN)
-    curses.curs_set(0)  # Hide cursor
-    screen.attron(curses.color_pair(3))
-    # header
+def header(screen, data, cols):
     screen.move(0, 0)
     screen.clrtoeol()
-    screen.chgat(0, 0, -1, curses.color_pair(3))
+    screen.attron(curses.color_pair(Theme.WHITE_ON_BLACK.value))
     steps: int = int(cols / 3)
     screen.addstr("Total attacks: ")
-    screen.addstr(f"{data.total:,}", curses.color_pair(2))
+    screen.addstr(f"{data.total:,}", curses.color_pair(Theme.RED_ON_BLACK.value))
     screen.move(0, 1 * steps)
     screen.addstr("Last: ")
-    screen.addstr(f"{data.last:%a, %b %d %H:%M:%S}", curses.color_pair(2))
+    screen.addstr(
+        f"{data.last:%a, %b %d %H:%M:%S}", curses.color_pair(Theme.RED_ON_BLACK.value)
+    )
     screen.move(0, 2 * steps)
     screen.addstr("Since: ")
-    screen.addstr(f"{data.first:%a, %b %d %H:%M:%S}", curses.color_pair(2))
+    screen.addstr(
+        f"{data.last:%a, %b %d %H:%M:%S}", curses.color_pair(Theme.RED_ON_BLACK.value)
+    )
     steps = int(cols / 6)
     screen.move(1, 0)
     screen.clrtoeol()
-    screen.chgat(1, 0, -1, curses.color_pair(4))
-    screen.attron(curses.color_pair(4))
+    screen.chgat(1, 0, -1, curses.color_pair(Theme.BLACK_ON_CYAN.value))
+    screen.attron(curses.color_pair(Theme.BLACK_ON_CYAN.value))
     screen.addstr("IP".rjust(steps - 2))
     screen.move(1, 1 * steps)
     screen.addstr("Location")
@@ -56,24 +60,61 @@ def draw_screen(screen, data, rows, cols):
     screen.addstr("Avg (/hour)".rjust(steps - 2))
     screen.move(1, 5 * steps)
     screen.addstr("Count".rjust(steps - 2))
-    screen.attroff(curses.color_pair(1))
-    # footer
+
+
+def footer(screen, rows):
     screen.move(rows - 1, 0)
     screen.clrtoeol()
-    screen.chgat(rows - 1, 0, -1, curses.color_pair(4))
+    screen.chgat(rows - 1, 0, -1, curses.color_pair(Theme.BLACK_ON_CYAN.value))
     screen.attron(curses.color_pair(4))
     screen.addstr("Q: Quit, T: Sort by time, F: Sort by frequency, N: Sort by Numbers")
+
+
+def panel(screen, data, rows, cols):
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    steps = int(cols / 6)
+    screen.move(2, 0)
+    screen.attron(curses.color_pair(1))
+    screen.chgat(rows - 1, 0, -1, curses.color_pair(1))
+    for idx, record in enumerate(data.records):
+        y = 2 + idx
+        screen.move(y, 0)
+        screen.clrtoeol()
+        screen.addstr(f"{record.ip:>15}".rjust(steps - 2), curses.color_pair(1))
+        screen.move(y, 2 * steps)
+        screen.addstr("🛇".rjust(steps - 5) if record.blocked else " ")
+        screen.move(y, 1 * steps)
+        screen.addstr(record.location)
+        screen.move(y, 3 * steps)
+        screen.addstr(f"{record.last:%b %d %H:%M:%S}")
+        screen.move(y, 4 * steps)
+        screen.addstr(f"{record.avg:>10,.0f}".rjust(steps - 2))
+        screen.move(y, 5 * steps)
+        screen.addstr(f"{record.num:>10,}\n".rjust(steps - 2))
+
+
+def draw_screen(screen, data, rows, cols):
+    # colors
+    curses.init_pair(Theme.WHITE_ON_BLACK.value, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(Theme.RED_ON_BLACK.value, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(Theme.CYAN_ON_BLACK.value, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(Theme.BLACK_ON_CYAN.value, curses.COLOR_BLACK, curses.COLOR_CYAN)
+    curses.start_color()
+    curses.curs_set(0)  # Hide cursor
+    header(screen, data, cols)
+    panel(screen, data, rows, cols)
+    footer(screen, rows)
     screen.refresh()
 
 
 def main(stdscr):
     mode: monitor.Mode = monitor.Mode.TIME
     source: monitor.Source = monitor.Source(db=DATABASE)
-    stdscr.nodelay(True)
+    stdscr.timeout(REFRESH)
     key_char: chr = ""
     while True:
-        data = source.get(mode)
         rows, cols = stdscr.getmaxyx()
+        data = source.get(mode, rows - 3)
         draw_screen(stdscr, data, rows, cols)
         key: int = stdscr.getch()
         # Convert the integer to its corresponding character
